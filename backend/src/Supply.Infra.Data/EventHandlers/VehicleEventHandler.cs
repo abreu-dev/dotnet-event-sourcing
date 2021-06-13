fa0d573c041407241e6441ee0eae9d8e592cf058
Gabriel Roberto de Abreu <gabriel.abreu@ambevtech.com.br>
2021-06-13 16:05:15 -0300

    Base of project, including add/update/remove for Vehicle, but event sourcing not implemented yet

---
+++ backend/src/Supply.Infra.Data/EventHandlers/VehicleEventHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Supply.Domain.Events.VehicleEvents;
+using Supply.Domain.Interfaces;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Supply.Infra.Data.EventHandlers
+{
+    public class VehicleEventHandler : 
+        INotificationHandler<VehicleAddedEvent>,
+        INotificationHandler<VehicleUpdatedEvent>,
+        INotificationHandler<VehicleRemovedEvent>
+    {
+        private readonly IVehicleRepository _vehicleRepository;
+
+        public VehicleEventHandler(IVehicleRepository vehicleRepository)
+        {
+            _vehicleRepository = vehicleRepository;
+        }
+
+        public async Task Handle(VehicleAddedEvent notification, CancellationToken cancellationToken)
+        {
+            var vehicle = await _vehicleRepository.GetById(notification.AggregateId);
+        }
+
+        public async Task Handle(VehicleUpdatedEvent notification, CancellationToken cancellationToken)
+        {
+            var vehicle = await _vehicleRepository.GetById(notification.AggregateId);
+        }
+
+        public async Task Handle(VehicleRemovedEvent notification, CancellationToken cancellationToken)
+        {
+            var vehicle = await _vehicleRepository.GetById(notification.AggregateId);
+        }
+    }
+}
